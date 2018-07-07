9281e3e6b2130bd7342c11c207e4bf9cd1b031fd
Will <yydragon.000@gmail.com>
2018-07-07 23:54:12 +0800

    Introduce JaCoCo
    - Introduce JaCoCo.
    - Add HomeController.java.

---
+++ src/main/java/net/whateverstart/medic/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package net.whateverstart.medic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody String home() {
+        return "This the home page";
+    }
+
+}
--- src/test/java/net/whateverstart/medic/ApplicationTest.java
+++ src/test/java/net/whateverstart/medic/ApplicationTest.java
@@ -2,17 +2,21 @@ package net.whateverstart.medic;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = GreetingController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTest {
 
     @Autowired
@@ -22,6 +26,12 @@ public class ApplicationTest {
     public void homePage() throws Exception {
         mockMvc.perform(get("/index.html"))
                 .andExpect(content().string(containsString("Get your greeting")));
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("home page")));
     }
 
 }
